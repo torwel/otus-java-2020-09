02c82e818e96837ed4f12567db12916cdb0be7a8
torwel <orwel@bk.ru>
2020-11-04 16:25:45 +0700

    Iter 03. Exchange commit.

---
+++ hw04-GC/src/main/java/ru/otus/torwel/BenchmarkMonitor.java
@@ -0,0 +1,46 @@
+package ru.otus.torwel;
+
+import com.sun.management.GarbageCollectionNotificationInfo;
+
+import javax.management.Notification;
+import javax.management.NotificationListener;
+import javax.management.openmbean.CompositeData;
+
+public class BenchmarkMonitor implements NotificationListener {
+
+    private long maxDurationSTW;
+    private long totalDurationSTW;
+
+    @Override
+    public void handleNotification(Notification notification, Object handback) {
+        if (notification.getType().equals(GarbageCollectionNotificationInfo.GARBAGE_COLLECTION_NOTIFICATION)) {
+            GarbageCollectionNotificationInfo info = GarbageCollectionNotificationInfo.from((CompositeData) notification.getUserData());
+
+/*
+            String gcName = info.getGcName();
+            String gcAction = info.getGcAction();
+            String gcCause = info.getGcCause();
+*/
+
+//            long startTime = info.getGcInfo().getStartTime();
+            long duration = info.getGcInfo().getDuration();
+            setMaxDurationSTW(duration);
+            totalDurationSTW = totalDurationSTW + duration;
+
+//            System.out.println("start:" + startTime + " Name:" + gcName + ", action:" + gcAction + ", gcCause:" + gcCause + "(" + duration + " ms)");
+        }
+    }
+
+    public long getMaxDurationSTW() {
+        return maxDurationSTW;
+    }
+
+    public long getTotalDurationSTW() {
+        return totalDurationSTW;
+    }
+
+    private void setMaxDurationSTW(long currentDuration) {
+        if (currentDuration > maxDurationSTW)
+            maxDurationSTW = currentDuration;
+    };
+}
--- hw04-GC/src/main/java/ru/otus/torwel/Launch.java
+++ hw04-GC/src/main/java/ru/otus/torwel/Launch.java
@@ -1,14 +1,18 @@
 package ru.otus.torwel;
 
-import javax.management.MBeanServer;
-import javax.management.ObjectName;
+import javax.management.*;
+import java.lang.management.GarbageCollectorMXBean;
 import java.lang.management.ManagementFactory;
 
 public class Launch {
 
+    final private static BenchmarkMonitor listener = new BenchmarkMonitor();
+
     public static void main(String[] args) throws Exception {
         System.out.println("Starting pid: " + ManagementFactory.getRuntimeMXBean().getName());
-//        switchOnMonitoring();
+
+        startGCMonitoring();
+
         long beginTime = System.currentTimeMillis();
 
         int size = 50_000;
@@ -20,9 +24,40 @@ public class Launch {
         Benchmark mbean = new Benchmark(loopCounter);
         mbs.registerMBean(mbean, name);
         mbean.setSize(size);
-        mbean.run();
+
+        try {
+            mbean.run();
+        } finally {
+//            System.out.println("Maximum duration STW: " + listener.getMaxDurationSTW());
+//            System.out.println("Total duration STWs: " + listener.getTotalDurationSTW());
+            System.out.println(listener.getMaxDurationSTW());
+            System.out.println(listener.getTotalDurationSTW());
+        }
+
 
         System.out.println("time:" + (System.currentTimeMillis() - beginTime) / 1000);
+
+
     }
+
+
+    private static void startGCMonitoring() {
+        for (GarbageCollectorMXBean gcbean : ManagementFactory.getGarbageCollectorMXBeans()) {
+            System.out.println("GC name:" + gcbean.getName());
+            try {
+                NotificationEmitter emitter = (NotificationEmitter) gcbean;
+                emitter.removeNotificationListener(listener);
+            } catch (ListenerNotFoundException e) {
+            }
+        }
+    }
+
+    private static void stopGCMonitoring() {
+        for (GarbageCollectorMXBean gcbean : ManagementFactory.getGarbageCollectorMXBeans()) {
+            NotificationEmitter emitter = (NotificationEmitter) gcbean;
+            emitter.addNotificationListener(listener, null, null);
+        }
+    }
+
 
 }
