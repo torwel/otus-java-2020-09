2d6cebcd138e7a39eadaeab0635f41f394152b9c
torwel <orwel@bk.ru>
2020-11-08 16:59:25 +0700

    Iter 07. Exchange commit.

--- hw04-GC/src/main/java/ru/otus/torwel/Launch.java
+++ hw04-GC/src/main/java/ru/otus/torwel/Launch.java
@@ -6,11 +6,11 @@ import org.slf4j.LoggerFactory;
 
 import javax.management.*;
 import javax.management.openmbean.CompositeData;
-import java.lang.management.GarbageCollectorMXBean;
-import java.lang.management.ManagementFactory;
-import java.lang.management.MemoryUsage;
+import java.lang.management.*;
 import java.text.SimpleDateFormat;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 //import java.util.logging.Logger;
 
 public class Launch {
@@ -33,7 +33,7 @@ public class Launch {
         startGCMonitoring();
 
         int size = 50_000;
-        int loopCounter = 10000;
+        int loopCounter = 20000;
         //int loopCounter = 100000;
         MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
         ObjectName name = new ObjectName("ru.otus:type=Benchmark");
@@ -45,8 +45,8 @@ public class Launch {
         try {
             mbean.run();
         } finally {
-            logger.debug(REPORT_MAX_DURARION_STW + listener.getMaxDurationSTW());
-            logger.debug(REPORT_TOTAL_DURARION_STWS + listener.getTotalDurationSTW());
+//            logger.debug(REPORT_MAX_DURARION_STW + listener.getMaxDurationSTW());
+//            logger.debug(REPORT_TOTAL_DURARION_STWS + listener.getTotalDurationSTW());
             logger.debug("work time: {} millis", (System.currentTimeMillis() - beginTime));
         }
 
@@ -64,7 +64,7 @@ public class Launch {
     }
 
     private static void startGCMonitoring() {
-        logger.info("startTime;duration;maxDuration;gcCause;gcName;gcAction");
+        logger.info("GC start time;GC duration;gcCause;gcName;gcAction");
         for (GarbageCollectorMXBean gcbean : ManagementFactory.getGarbageCollectorMXBeans()) {
             NotificationEmitter emitter = (NotificationEmitter) gcbean;
             emitter.addNotificationListener(listener, null, null);
@@ -73,8 +73,8 @@ public class Launch {
 
     public static class BenchmarkMonitor implements NotificationListener {
 
-        private volatile long maxDurationSTW;
-        private volatile long totalDurationSTW;
+//        private volatile long maxDurationSTW;
+//        private volatile long totalDurationSTW;
 
         @Override
         public void handleNotification(Notification notification, Object handback) {
@@ -87,51 +87,82 @@ public class Launch {
 
                 long startTime = info.getGcInfo().getStartTime();
                 long duration = info.getGcInfo().getDuration();
-                setMaxDurationSTW(duration);
-                totalDurationSTW = totalDurationSTW + duration;
+//                setMaxDurationSTW(duration);
+//                totalDurationSTW += duration;
 
+/*
                 Map<String, MemoryUsage> memBefore = info.getGcInfo().getMemoryUsageBeforeGc();
                 Map<String, MemoryUsage> memAfter = info.getGcInfo().getMemoryUsageAfterGc();
                 for (Map.Entry<String, MemoryUsage> entry : memAfter.entrySet()) {
-                    String name = entry.getKey();
+                    String memType = entry.getKey();
                     MemoryUsage memdetail = entry.getValue();
                     long memInit = memdetail.getInit();
                     long memCommitted = memdetail.getCommitted();
                     long memMax = memdetail.getMax();
                     long memUsed = memdetail.getUsed();
-                    MemoryUsage before = memBefore.get(name);
-
-/*
-                    var beforeCommited = before.getCommitted();
-                    long beforepercent = 0;
-                    long percent = 0; //>100% when it gets expanded
-                    if (beforeCommited > 0) {
-                        beforepercent = ((before.getUsed() * 1000L) / beforeCommited);
-                        percent = ((memUsed * 1000L) / beforeCommited); //>100% when it gets expanded
-                    }
-*/
+                    MemoryUsage before = memBefore.get(memType);
                     long beforepercent = ((before.getUsed()*1000L)/before.getCommitted());
                     long percent = ((memUsed*1000L)/before.getCommitted()); //>100% when it gets expanded
-                    System.out.print(name + (memCommitted==memMax?"(fully expanded)":"(still expandable)") +"used: "+(beforepercent/10)+"."+(beforepercent%10)+"%->"+(percent/10)+"."+(percent%10)+"%("+((memUsed/1048576)+1)+"MB)\n");
+                    System.out.print(memType + (memCommitted==memMax?"(fully expanded)":"(still expandable)") +"used: "+(beforepercent/10)+"."+(beforepercent%10)+"%->"+(percent/10)+"."+(percent%10)+"%("+((memUsed/1048576)+1)+"MB)\n");
                 }
+*/
 
+                for(MemoryPoolMXBean mBean: ManagementFactory.getMemoryPoolMXBeans()) {
+                    System.out.println(mBean.getName() + " - " + mBean.getType());
+                    //TODO: Здесь составляем список mem с типами памяти MemoryType.HEAP
+                    }
+                System.out.println();
 
-                logger.info("{};{};{};{};{};{}", startTime, duration, maxDurationSTW, gcCause, gcName, gcAction);
+                // TODO: Далее для mapBefore и mapAfter посчитать сумму память HEAP типов
+                // и вывести в лог в нужном формате
+
+                Map mapBefore = info.getGcInfo().getMemoryUsageBeforeGc();
+                Map mapAfter = info.getGcInfo().getMemoryUsageAfterGc();
+                System.out.println("Before GC Memory Usage Details....");
+                Set memType = mapBefore.keySet();
+                Iterator it = memType.iterator();
+                while(it.hasNext()) {
+                    String type = (String)it.next();
+                    System.out.println(type);
+                    MemoryUsage mu1 = (MemoryUsage) mapBefore.get(type);
+                    System.out.print("Initial Size: "+mu1.getInit());
+                    System.out.print(" Used: "+ mu1.getUsed());
+                    System.out.print(" Max: "+mu1.getMax());
+                    System.out.print(" Committed: "+mu1.getCommitted());
+                    System.out.println("");
+                }
+                System.out.println();
+                System.out.println("After GC Memory Usage Details....");
+                memType = mapAfter.keySet();
+                it = memType.iterator();
+                while(it.hasNext()) {
+                    String type = (String)it.next();
+                    System.out.println(type);
+                    MemoryUsage mu2 = (MemoryUsage) mapAfter.get(type);
+                    System.out.print("Initial Size: "+mu2.getInit());
+                    System.out.print(" Used: "+ mu2.getUsed());
+                    System.out.print(" Max: "+mu2.getMax());
+                    System.out.print(" Committed: "+mu2.getCommitted());
+                    System.out.println("");
+                }
+                System.out.println();
+
+                logger.info("{};{};{};{};{}", startTime, duration, gcCause, gcName, gcAction);
                 logger.debug("start:" + startTime + " Name:" + gcName + ", action:" + gcAction + ", gcCause:" + gcCause + "(" + duration + " ms)");
             }
         }
 
-        public long getMaxDurationSTW() {
-            return maxDurationSTW;
-        }
+//        public long getMaxDurationSTW() {
+//            return maxDurationSTW;
+//        }
 
-        public long getTotalDurationSTW() {
-            return totalDurationSTW;
-        }
+//        public long getTotalDurationSTW() {
+//            return totalDurationSTW;
+//        }
 
-        private void setMaxDurationSTW(long currentDuration) {
-            if (currentDuration > maxDurationSTW)
-                maxDurationSTW = currentDuration;
-        };
+//        private void setMaxDurationSTW(long currentDuration) {
+//            if (currentDuration > maxDurationSTW)
+//                maxDurationSTW = currentDuration;
+//        };
     }
 }
