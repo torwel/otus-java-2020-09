0172de73b319cedba3a1b8d13d9b747b9c03b887
torwel <orwel@bk.ru>
2020-12-03 23:03:09 +0700

    Fixed code performance.

--- hw05-autoLogging/src/main/java/ru/otus/torwel/Ioc.java
+++ hw05-autoLogging/src/main/java/ru/otus/torwel/Ioc.java
@@ -3,6 +3,8 @@ package ru.otus.torwel;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Ioc {
 
@@ -43,11 +45,13 @@ public class Ioc {
     }
 
     /**
-     * Класс-обработчик, наоснове которого создается прокси-объект.
+     * Класс-обработчик, на основе которого создается прокси-объект.
      * Содержит оригинальный объект `Car`.
      */
     private static class CarInvocationHandler implements InvocationHandler {
         private final CarLogging car;
+        private List<Method> methodsForLogging = new ArrayList<>();
+        private List<Method> methodsExcludeLogging = new ArrayList<>();
 
         CarInvocationHandler(CarLogging car) {
             this.car = car;
@@ -66,29 +70,57 @@ public class Ioc {
         @Override
         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
 
-            // Реализация логирования методов, отмеченных аннотацией @Log
-            // Формат вывода:
-            // "executed method: methodName, param1: 1, param2: 2"
-            // "executed method: methodName, param: 1"
-            // "executed method: methodName"
-
+/*
+            // Логирование методов, отмеченных аннотацией @Log без использования
+            // кэширования вызванных ранее методов
             if (method.isAnnotationPresent(Log.class)) {
-                StringBuilder sb = new StringBuilder(0);
-                sb.append("executed method: ").append(method.getName());
-                if (args != null) {
-                    if (args.length == 1) {
-                        sb.append(", param: ").append(args[0]);
-                    } else if (args.length > 1) {
-                        for (int i = 0; i < args.length; i++) {
-                            sb.append(", param").append(i + 1).append(": ").append(args[i]);
-                        }
+                System.out.println(buildLogMessage(method, args));
+            }
+*/
+
+            // Логирование методов, отмеченных аннотацией @Log с использованием
+            // кэширования вызванных ранее методов
+            if (methodsForLogging.contains(method)) {
+                System.out.println(buildLogMessage(method, args));
+            }
+            else {
+                if (!methodsExcludeLogging.contains(method)) {
+                    if (method.isAnnotationPresent(Log.class)) {
+                        methodsForLogging.add(method);
+                    }
+                    else {
+                        methodsExcludeLogging.add(method);
                     }
                 }
-                System.out.println(sb.toString());
             }
 
             // Выполнение метода оригинального объекта
             return method.invoke(car, args);
+        }
+
+        /**
+         * Метод создает строку для вывода в лог.
+         * Формат вывода:
+         * "executed method: methodName, param1: 1, param2: 2"
+         * "executed method: methodName, param: 1"
+         * "executed method: methodName"
+         * @param method метод интерфейса, который был вызван на прокси-объекте.
+         * @param args массив параметров вызванного метода.
+         * @return строка для логирования
+         */
+        private String buildLogMessage(Method method, Object[] args) {
+            StringBuilder sb = new StringBuilder(0);
+            sb.append("executed method: ").append(method.getName());
+            if (args != null) {
+                if (args.length == 1) {
+                    sb.append(", param: ").append(args[0]);
+                } else if (args.length > 1) {
+                    for (int i = 0; i < args.length; i++) {
+                        sb.append(", param").append(i + 1).append(": ").append(args[i]);
+                    }
+                }
+            }
+            return sb.toString();
         }
     }
 }
--- hw05-autoLogging/src/main/java/ru/otus/torwel/Launch.java
+++ hw05-autoLogging/src/main/java/ru/otus/torwel/Launch.java
@@ -3,19 +3,45 @@ package ru.otus.torwel;
 public class Launch {
 
     public static void main(String[] args) {
+        // Количество  циклов тестирования
+        int testCount = 10000;
+
+        // Здесь начинаем тестирование оригинального объекта
+        final long checkpoint1 = System.nanoTime();
 
         Car car = new Car();
-        car.setModel("Жига");
-        car.setWeight(1200);
-        car.setHeight(199);
 
-        CarLogging proxyCar = Ioc.createCar(car);
+        final long checkpoint2 = System.nanoTime();
 
-        proxyCar.modifyValues(1200, 160, "Ока");
-        proxyCar.modifyValues(1050, 140);
-        proxyCar.modifyValues(850);
+        for (int i = 0; i < testCount; i++ ) {
+            car.modifyValues(9000+i, 160+i, "Жига+ " + i);
+            car.modifyValues(7000+i, 150+i);
+            car.modifyValues(199+i);
+            System.out.println(car.getModel());
+        }
 
-        System.out.println(proxyCar.getModel());
+        final long checkpoint3 = System.nanoTime();
+
+        // Здесь начинаем тестирование прокси-объекта
+        // Оригинальный объект используем новый
+        CarLogging proxyCar = Ioc.createCar();
+
+        final long checkpoint4 = System.nanoTime();
+
+        for (int i = 0; i < testCount; i++ ) {
+            proxyCar.modifyValues(1200+i, 160+i, "Ока_" + i);
+            proxyCar.modifyValues(1050+i, 140+i);
+            proxyCar.modifyValues(850+i);
+            System.out.println(proxyCar.getModel());
+        }
+
+        final long checkpoint5 = System.nanoTime();
+
+        System.out.println("Preparing original object (microsec): " + ((checkpoint2 - checkpoint1) / 1000) );
+        System.out.println("Execution original time (microsec): " + ((checkpoint3 - checkpoint2) / 1000) );
+
+        System.out.println("Preparing proxy object (microsec): " + (checkpoint4 - checkpoint3) / 1000);
+        System.out.println("Execution proxy time (microsec): " + ((checkpoint5 - checkpoint4) / 1000) );
 
     }
 }
